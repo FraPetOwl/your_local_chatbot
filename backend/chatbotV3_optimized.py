b177aba990a13097d231a85b357e9056efccbd2a
Peter Fraser <peter.fraser1@ibm.com>
2025-09-09 12:54:55 -0400

    deleted local host mention completely

--- backend/chatbotV3_optimized.py
+++ backend/chatbotV3_optimized.py
@@ -108,7 +108,6 @@ if os.getenv("LLM_ENABLED", "1") != "0" and Llama is not None:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
         "http://158.101.102.126"   
     ],
     allow_credentials=True,
